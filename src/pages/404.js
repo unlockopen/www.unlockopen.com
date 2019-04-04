ab7f36f1d426da60db6b5b85b5ae96eea3f790b4
Tobie Langel <tobie@unlockopen.com>
2019-04-04 23:38:56 +0200

    Fix 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Layout from '../components/Layout'
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
+  <Layout>
+    <Helmet title="UnlockOpen | Page Not Found" />
+    <h1>Page Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist. Sorry. :(</p>
-  </div>
+  </Layout>
 )
 
 export default NotFoundPage
