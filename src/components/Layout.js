b032ce3e7d8b40ba91c640ab9813537dc9ec6f6f
Tobie Langel <tobie@unlockopen.com>
2019-04-05 10:13:08 +0200

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,6 @@
 import React from 'react'
-import Helmet from 'react-helmet'
+import BasicLayout from './BasicLayout'
 //import Hero from './Hero'
-import Header from './Header'
-import Footer from './Footer'
-import ColorLine from './ColorLine'
 import MailinglistForm from './MailinglistForm'
 import Bio from './Bio'
 
@@ -14,36 +11,14 @@ class Layout extends React.Component {
     const children = this.props.children;
     let bio = this.props.bio ? <Bio /> : null;
     return (
-
-        <div>
-            <Helmet>
-                <html lang="en" />
-                <meta charSet="utf-8" />
-            </Helmet>
-           <Header />
-            <div style={{
-                marginLeft: 'auto',
-                marginRight: 'auto',
-                maxWidth: rhythm(24),
-                padding: `${rhythm(1.5)} ${rhythm(3 / 4)} ${rhythm(1)} ${rhythm(3 / 4)}`,
-            }}>
-                { children }
-                <MailinglistForm>
-                  <p><strong>Intrigued? Want to know more?</strong></p>
-                  <p>Subscribe to our mailing list.</p>
-                </MailinglistForm>
-
-                {bio}
-            </div>
-            <Footer style={{
-                marginLeft: 'auto',
-                marginRight: 'auto',
-                maxWidth: rhythm(24),
-                padding: `${rhythm(1)} ${rhythm(3 / 4)} 0 ${rhythm(3 / 4)}`,
-            }} />
-            <ColorLine />
-        </div>
-        
+        <BasicLayout>
+            { children }
+            <MailinglistForm>
+              <p><strong>Intrigued? Want to know more?</strong></p>
+              <p>Subscribe to our mailing list.</p>
+            </MailinglistForm>
+            {bio}
+        </BasicLayout>
     )
   }
 }
