320d623c4e63778d70780a8065501aaab38e5a59
Tobie Langel <tobie@unlockopen.com>
2019-04-05 10:28:42 +0200

    Update info.js

--- src/pages/info.js
+++ src/pages/info.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import Helmet from 'react-helmet'
 
 import { rhythm, scale, options} from '../utils/typography'
 import * as brand from '../utils/brand'
